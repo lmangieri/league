613452247d34da903b275d23fe59555f0e6d49ff
Leandro Mangieri <leandro.mangieri02@gmail.com>
2018-09-02 20:08:23 -0300

    t

--- src/main/java/leandroportfolio/league/rest/HelloWorldResource.java
+++ src/main/java/leandroportfolio/league/rest/HelloWorldResource.java
@@ -5,12 +5,20 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import leandroportfolio.league.dao.PlayerDAO;
+import leandroportfolio.league.model.Player;
+
 @Path("/hello")
 public class HelloWorldResource {
 
 	@GET
 	@Path("/{param}")
 	public Response getMsg(@PathParam("param") String msg) {
+        System.out.println( "Hello World!" );
+        PlayerDAO dao = new PlayerDAO();
+        for(Player p : dao.getAllPlayers()) {
+        	System.out.println("Player name = "+ p.getName());
+        }
 
 		String output = "Jersey sayyy : " + msg;
 
