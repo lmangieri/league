793d3d4238eac81c108731e7a7326be259f5284f
Leandro Mangieri <leandro.mangieri02@gmail.com>
2018-10-28 14:37:58 -0300

    Adding second cors test

---
+++ src/main/java/leandroportfolio/league/cors/FilterTest.java
@@ -0,0 +1,30 @@
+package leandroportfolio.league.cors;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class FilterTest extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request,
+			HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+	    response.addHeader("Access-Control-Allow-Origin", "*");
+	    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+	    response.setHeader("Access-Control-Allow-Credentials", "true");
+	    response.setHeader("Access-Control-Allow-Headers",
+	            "content-type, x-gwt-module-base, x-gwt-permutation, clientid, longpush");
+
+	    filterChain.doFilter(request, response);
+		
+	}
+
+}
